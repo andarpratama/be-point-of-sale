5e67cd606d80da2f1ce695683ae0a70f9893480c
wdumalang <welington2686@gmail.com>
2021-05-22 23:10:51 +0800

    test CRUD inventoryBrand

--- src/controllers/inventory.brand.controller.ts
+++ src/controllers/inventory.brand.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { BrandModel } from "../models/brand.model";
 
 class InventoryBrandController {
     static getInventoryBrand(req: Request, res: Response) {
@@ -9,21 +10,61 @@ class InventoryBrandController {
             message: "Get Inventory Brand Data",
         });
     }
-    static postInventoryBrand(req: Request, res: Response) {
-        res.status(201).json({
-            success: true,
-            statusCode: 201,
-            responseStatus: "Status OK",
-            message: "Inventory Brand Created",
-        });
+    static async postInventoryBrand(req: Request, res: Response) {
+        const name = req.body.name;
+
+        try {
+            if (!name) {
+                res.status(403).json({
+                    success: false,
+                    statusCode: 403,
+                    responseStatus: "Status not OK",
+                    message: "Please Fill the Brand Name",
+                });
+            } else {
+                const newBrand = await BrandModel.create({
+                    name: name,
+                });
+                res.status(201).json({
+                    success: true,
+                    statusCode: 201,
+                    responseStatus: "Status OK",
+                    message: `Brand ${name} Created`,
+                    data: newBrand,
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                statusCode: 500,
+                responseStatus: "Status not OK",
+                message: "Failed to Create Brand Name",
+            });
+        }
     }
-    static editInventoryBrand(req: Request, res: Response) {
-        res.status(200).json({
-            success: true,
-            statusCode: 200,
-            responseStatus: "Status OK",
-            message: "Edit Inventory Brand",
-        });
+    static async editInventoryBrand(req: Request, res: Response) {
+        const brandID = req.params.id;
+        const name = req.body.name;
+
+        try {
+            const updateName = await BrandModel.findByIdAndUpdate(brandID, {
+                name: name,
+            });
+            res.status(200).json({
+                success: true,
+                statusCode: 200,
+                responseStatus: "Status OK",
+                message: `Success edit brand name to ${updateName?.name}`,
+                data: updateName,
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                statusCode: 500,
+                responseStatus: "Status not OK",
+                message: "Update Brand Name Failed",
+            });
+        }
     }
     static deleteInventoryBrand(req: Request, res: Response) {
         res.status(200).json({
