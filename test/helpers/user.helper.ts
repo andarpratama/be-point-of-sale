4ed7ecca95d228e3b5d905e9ed42193d48a870e6
imarthaid <master.amarta@gmail.com>
2021-05-22 20:14:15 +0700

    edit error unit testing

--- test/helpers/user.helper.ts
+++ test/helpers/user.helper.ts
@@ -4,7 +4,7 @@ import { APP } from '../../src/app';
 // COMMENT: Valid endpoint
 const infoUser = async (userId: string, bearerToken: string) => {
     const userInfo = await request(APP)
-        .get(`/api/v1/user/detail/${userId}`)
+        .get(`/api/v1/user/${userId}/detail`)
         .set('Authorization', `${bearerToken}`)
     return userInfo;
 };
